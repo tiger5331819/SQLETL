9965fa1bfb39281c02b81a52100221584386d76f
suhuyuan <tiger5331819@163.com>
2020-12-04 17:53:19 +0800

    更新核心

--- SQLETL/ETL/Core/ETLService.cs
+++ SQLETL/ETL/Core/ETLService.cs
@@ -1,34 +1,63 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SQLETL.ETL.Core
 {
-    public interface IETLService 
+    public interface IETLService
     {
         public void Start(object stateInfo);
     }
 
-    public abstract class ETLService<IEntity, ISource> :IETLService where IEntity : class, new()
+    public abstract class ETLService<IEntity, ISource> : IETLService where IEntity : class, new()
     {
         private readonly string name;
-        protected ETLService(string name) 
+        protected ETLService(string name)
         {
             this.name = name;
         }
         public void Start(object stateInfo)
         {
             List<ISource> sourceData = GetSourceData();
-            List<IEntity> entityList = new List<IEntity>(sourceData.Count);
-            foreach(var item in sourceData)
+
+            Console.WriteLine(name+"数据抽取完成！");
+
+            List<Task<IEntity>> DataTransferTasks = new List<Task<IEntity>>();
+            foreach (var item in sourceData)
             {
-                var data = DataTransfer(item);
-                if (data == null) continue;
-                entityList.Add(data);
+                DataTransferTasks.Add(DataTransfer(item));
             }
-            SaveData(entityList);
+            var entityList = Task.WhenAll(DataTransferTasks);
+            while (entityList.Status != TaskStatus.RanToCompletion)
+            {
+                var completedCount = (double)DataTransferTasks.Where(task => task.IsCompleted).Count();
+                Console.WriteLine(name + "数据转换已完成：" + (completedCount / (double)DataTransferTasks.Count) * 100 + "%");
+
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine(name+"数据转换完成！");
+
+
+            List<Task> SaveDataTasks = new List<Task>();
+            foreach (var item in entityList.Result)
+            {
+                SaveDataTasks.Add(SaveData(item));
+            }
+
+            var SaveDataTaskList = Task.WhenAll(SaveDataTasks);
+            while (SaveDataTaskList.Status != TaskStatus.RanToCompletion)
+            {
+                var completedCount = (double)SaveDataTasks.Where(task => task.IsCompleted).Count();
+                Console.WriteLine(name + "数据保存已完成：" + (completedCount / (double)SaveDataTasks.Count) * 100 + "%");
+
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine(name+"数据保存完成！");
+
             Console.WriteLine(name + " 执行完成");
         }
         /// <summary>
@@ -42,10 +71,10 @@ namespace SQLETL.ETL.Core
         /// <typeparam name="ISource"></typeparam>
         /// <param name="source"></param>
         /// <returns></returns>
-        protected abstract IEntity DataTransfer(ISource source);
+        protected abstract Task<IEntity> DataTransfer(ISource source);
         /// <summary>
         /// 装载数据
         /// </summary>
-        protected abstract void SaveData(List<IEntity> entityList);
+        protected abstract Task SaveData(IEntity entity);
     }
 }
